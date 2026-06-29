927c3f032393d5b007205f9e124f90384b4cc01c
keiner <keinerposada0402@gmail.com>
2026-06-28 20:44:09 -0500

    Fix: agregado LOGIN_URL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -101,6 +101,7 @@ else:
     }
 
 AUTH_USER_MODEL = 'authentication.User'
+LOGIN_URL = '/login/'
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
